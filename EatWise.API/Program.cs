cc493f6978e7c36bf019efdfb7d852c8d5b15e1b
defmindat <defmindat@gmail.com>
2025-06-19 12:03:08 +0700

    Add healthcheck

--- EatWise.API/Program.cs
+++ EatWise.API/Program.cs
@@ -1,23 +1,29 @@
 using EatWise.API.Extensions;
+using EatWise.API.Middleware;
 using EatWise.Common.Application;
 using EatWise.Common.Infrastructure;
 using EatWise.Common.Presentation.Endpoints;
 using EatWise.Harvester.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.ReadFrom.Configuration(context.Configuration));
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 
 string databaseConnectionString = builder.Configuration.GetConnectionString("Database")!;
 
 builder.Services.AddInfrastructure(databaseConnectionString);
 
 builder.Configuration.AddModuleConfiguration(["harvesters"]);
+
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(databaseConnectionString);
 
 builder.Services.AddHarvesterModule(builder.Configuration);
 
@@ -41,6 +47,13 @@ if (app.Environment.IsDevelopment())
 
 app.MapEndpoints();
 
+app.MapHealthChecks("health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
 app.UseSerilogRequestLogging();
+
+app.UseExceptionHandler();
 
 app.Run();
